2415c8591e91c9215edbc6e9623e5959c9f6ad06
viniknoxville <ass.marcosribeiro@gmail.com>
2022-03-09 01:43:18 -0300

    Exemplo Aula 02

---
+++ Projeto_Integrador_2/ExemploJanelas/src/main/java/com/mycompany/exemplojanelas/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemplojanelas;
+
+/**
+ *
+ * @author Marcos
+ */
+public class NewClass {
+    TelaPrincipal telaInicial = new TelaPrincipal();
+    telaInicial.setVisible(true);
+}
