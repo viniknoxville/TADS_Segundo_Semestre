ae526d050be1a0e151b8894f595a7460f64c34f0
viniknoxville <ass.marcosribeiro@gmail.com>
2022-03-22 01:45:02 -0300

    Atualização 01

---
+++ Algoritimos_e_Programação_2/Lista_de_Exercícios_01/BEE_1173.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lista_de_Exercícios_01;
+
+/**
+ *
+ * @author Marcos
+ */
+public class BEE_1173 {
+    
+}
---
+++ Algoritimos_e_Programação_2/Lista_de_Exercícios_01/BEE_1180.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lista_de_Exercícios_01;
+
+/**
+ *
+ * @author Marcos
+ */
+    import java.io.IOException ;
+    import java.util.Scanner ;
+
+    public class BEE_1180 {
+
+        public static void main(String[] args) throws IOException {
+            Scanner leitor = new Scanner(System.in);
+            int N = leitor.nextInt();
+            int[] X = new int[N];
+            int menor = 0, posMenor = 0;
+            for (int i = 0; i < N; i++) {
+                X[i] = leitor.nextInt();
+                if (i == 0) {
+                    menor = X[i];
+                    posMenor = i;
+                } else if (X[i] < menor) {
+                    menor = X[i];
+                    posMenor = i;
+                }
+            }
+            System.out.println("Menor valor: " + menor);
+            System.out.println("Posicao: " + posMenor);
+        }
+
+    }
+
